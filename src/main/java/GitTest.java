694cd189460f5e2783ffeb79ff7f364227f1c82f
guole <1198611866@qq.com>
2021-08-12 13:57:49 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: GL
+ * @create: 2021-08-12 13:54
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
